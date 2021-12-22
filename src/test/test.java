b94f9f011ed8801ae977361e30d4e449340296fa
huxiansheng <huxiansheng.com>
2021-12-22 16:54:14 +0800

    thired commit

--- src/test/test.java
+++ src/test/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("ff");
         System.out.println("ffg");
+        System.out.println("ffg3");
     }
 }
